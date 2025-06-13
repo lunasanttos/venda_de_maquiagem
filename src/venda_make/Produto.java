ae02a8625acf32d4e86b17ef63a8ce40d1c53b37
luna santos <oluna8466@gmail.com>
2025-06-13 20:09:42 -0300

    Create class produto

---
+++ src/venda_make/Produto.java
@@ -0,0 +1,28 @@
+package venda_make;
+
+public abstract class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+       this.preco = preco;
+    }
+
+
+    public abstract String toString();
+}
